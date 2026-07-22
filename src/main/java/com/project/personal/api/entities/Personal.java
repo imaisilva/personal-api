714094dca2670d41a09688b132c8ca129e250e5e
imaisilva <alohasp@outlook.com>
2026-07-22 11:02:03 -0300

    Personal

---
+++ src/main/java/com/project/personal/api/entities/Personal.java
@@ -0,0 +1,101 @@
+package com.project.personal.api.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Personal implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private User user;
+	
+	private String name;
+	private String specialty;
+	private String profilePhoto;
+	private String biography;
+	
+	public Personal() {
+	}
+
+	public Personal(Long id, User user, String name, String specialty, String profilePhoto, String biography) {
+		this.id = id;
+		this.user = user;
+		this.name = name;
+		this.specialty = specialty;
+		this.profilePhoto = profilePhoto;
+		this.biography = biography;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public String getProfilePhoto() {
+		return profilePhoto;
+	}
+
+	public void setProfilePhoto(String profilePhoto) {
+		this.profilePhoto = profilePhoto;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Personal other = (Personal) obj;
+		return Objects.equals(id, other.id);
+	}
+}
